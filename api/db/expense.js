bde84f6facd78f55ab42e68773b907931212152d
Pablo Mendez <mendez.developer@gmail.com>
2018-10-17 19:19:47 -0300

    added expense db methods

---
+++ api/db/expense.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Expense = require('../models/expense');
+
+exports.create = expenseData => {
+  const expense = new Expense({
+    _id: new mongoose.Types.ObjectId(),
+    ...expenseData
+  });
+
+  return expense.save();
+}
+
+exports.getByAccountId = (accountId) => {
+  return Expense.find({ accountId });
+}
