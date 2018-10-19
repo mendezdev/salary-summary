8873aa94a7ddd30a465c181db398ec2fa4a24711
Pablo Mendez <mendez.developer@gmail.com>
2018-10-19 16:09:17 -0300

    added middleware to routes account

--- api/routes/account.js
+++ api/routes/account.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
+const checkAuth = require('../middlewares/check-auth');
 const AccountController = require('../controllers/accounts');
 
-router.get('/', AccountController.get);
-router.post('/', AccountController.create);
-router.get('/:id', AccountController.getById);
-router.put('/:id', AccountController.update);
-router.get('/:id/expenses', AccountController.getAllExpenses);
-router.post('/:id/expenses', AccountController.createExpense);
+router.get('/', checkAuth, AccountController.get);
+router.post('/', checkAuth, AccountController.create);
+router.get('/:id', checkAuth, AccountController.getById);
+router.put('/:id', checkAuth, AccountController.update);
+router.get('/:id/expenses', checkAuth, AccountController.getAllExpenses);
+router.post('/:id/expenses', checkAuth, AccountController.createExpense);
 
 module.exports = router;
